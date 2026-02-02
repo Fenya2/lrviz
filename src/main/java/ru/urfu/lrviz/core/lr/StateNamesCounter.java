3b948ea3269ce5b55b2eb55c756ec5c1ac7b47ec
fenya <fenya74.09@gmail.com>
2026-02-02 22:38:35 +0500

    Тесты на lr0

--- src/main/java/ru/urfu/lrviz/core/lr/StateNamesCounter.java
+++ src/main/java/ru/urfu/lrviz/core/lr/StateNamesCounter.java
@@ -1,29 +1,25 @@
 package ru.urfu.lrviz.core.lr;
 
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Генератор имен состояний автомата
  */
-@Service
 public class StateNamesCounter {
-    private final ScopedValue<Map<String, Integer>> nextNumbers = ScopedValue.newInstance();
+    private final Map<String, Integer> stateCount;
 
-    public void execute(Runnable runnable) {
-        ScopedValue.where(nextNumbers, new HashMap<>()).run(runnable);
+    public StateNamesCounter() {
+        this.stateCount = new HashMap<>();
     }
 
     public int nextNumber(String stateName) {
-        Map<String, Integer> sessionMap = nextNumbers.get();
-        if (!sessionMap.containsKey(stateName)) {
-            sessionMap.put(stateName, 2);
+        if (!stateCount.containsKey(stateName)) {
+            stateCount.put(stateName, 2);
             return 1;
         }
-        int result = sessionMap.get(stateName);
-        sessionMap.put(stateName, result + 1);
+        int result = stateCount.get(stateName);
+        stateCount.put(stateName, result + 1);
         return result;
     }
 }
