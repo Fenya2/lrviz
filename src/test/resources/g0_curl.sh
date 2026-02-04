3f7da4bf8cb1e4fb1023a27dc76ebc90364b5d45
fenya <fenya74.09@gmail.com>
2026-02-05 02:09:57 +0500

    Добавил документацию через Spring REST Docs

--- src/test/resources/g0_curl.sh
+++ src/test/resources/g0_curl.sh
@@ -1,14 +1,14 @@
-curl -X POST http://localhost:8080/build/lr0 \
+curl -X POST http://localhost:8080/api/build/lr0 \
   -H "Content-Type: application/json" \
-  -d '{
-    "terminals": ["i", "r", "a", ";"],
-    "nonTerminals": ["D", "T", "L"],
-    "rules": [
-      { "left": "D", "right": "TL" },
-      { "left": "T", "right": "i" },
-      { "left": "T", "right": "r" },
-      { "left": "L", "right": "L;a" },
-      { "left": "L", "right": "a" }
-    ],
-    "startSymbol": "D"
+    -d '{
+  "terminals" : [ "(", ")" ],
+  "nonTerminals" : [ "S" ],
+  "rules" : [ {
+    "left" : "S",
+    "right" : "(S)"
+  }, {
+    "left" : "S",
+    "right" : ""
+  } ],
+  "startSymbol" : "S"
   }' | jq
