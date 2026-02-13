cfab777fb74dd86dbeda79c06e9ec35cea4d4bf4
fenya <fenya74.09@gmail.com>
2026-02-13 12:39:34 +0500

    Удалил лишние классы

--- src/main/java/ru/urfu/lrviz/api/dto/LRDFADto.java
+++
@@ -1,9 +0,0 @@
-package ru.urfu.lrviz.api.dto;
-
-import java.util.List;
-import java.util.Map;
-
-public record LRDFADto(
-        List<LRStateDto> states,
-        Map<TransitionDto, String> transitions){
-}
--- src/main/java/ru/urfu/lrviz/api/dto/OperationDetails.java
+++
@@ -1,9 +0,0 @@
-package ru.urfu.lrviz.api.dto;
-
-/**
- *
- * @author fenya
- * @since 03.02.2026
- */
-public interface OperationDetails {
-}
