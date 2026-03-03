a33f86d6e173538653d4cfae203d8c61c3b09243
fenya <fenya74.09@gmail.com>
2026-03-04 03:25:55 +0500

    Вынес общую часть документации

--- src/test/java/ru/urfu/lrviz/api/rest/docs/BuildLR1DocsTest.java
+++ src/test/java/ru/urfu/lrviz/api/rest/docs/BuildLR1DocsTest.java
@@ -1,18 +1,23 @@
 package ru.urfu.lrviz.api.rest.docs;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import java.util.stream.Stream;
+
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.urfu.lrviz.api.VersionsConstants.V1;
-import static ru.urfu.lrviz.api.rest.docs.DocumentationConstants.GRAMMAR_DTO;
+import static ru.urfu.lrviz.api.rest.docs.DocumentationConstants.*;
 import static ru.urfu.lrviz.core.GrammarExamples.G_6;
 import static ru.urfu.lrviz.core.GrammarExamples.getJsonDto;
 
@@ -39,83 +44,22 @@ class BuildLR1DocsTest extends AbstractMethodDocsTest {
         return DOCUMENTED_PATH;
     }
 
+    @Override
+    protected String getProduces() {
+        return APPLICATION_JSON_VALUE;
+    }
+
     @Test
     void document() throws Exception {
         this.mockMvc.perform(post(DOCUMENTED_PATH, V1)
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(getJsonDto(G_6)))
                 .andExpect(status().isOk())
                 .andDo(MockMvcRestDocumentation.document(getSnippetPath(), preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-                        requestHeaders(
-                                headerWithName(HttpHeaders.ACCEPT).description("Всегда `%s`".formatted(MediaType.APPLICATION_JSON_VALUE))
-                        ),
+                        requestHeaders(ACCEPT_JSON_HEADER),
                         requestFields(GRAMMAR_DTO),
-                        responseHeaders(
-                                headerWithName(HttpHeaders.CONTENT_TYPE).description("Всегда %s".formatted(MediaType.APPLICATION_JSON))
-                        ),
-                        responseFields(
-                                fieldWithPath("automaton").description("Построенный <<automaton,LR(0)-автомат>>"),
-                                fieldWithPath("automaton.states").description("<<automatonStates,Состояния>> автомата"),
-                                fieldWithPath("automaton.states[].name").description("Имя состояния"),
-                                fieldWithPath("automaton.states[].items").description("<<lrItems,LR(0)-пункты>> в состоянии"),
-                                fieldWithPath("automaton.states[].items[].rule")
-                                        .description("Правило грамматики пункта"),
-                                fieldWithPath("automaton.states[].items[].rule.left")
-                                        .description("Левая часть правила"),
-                                fieldWithPath("automaton.states[].items[].rule.right")
-                                        .description("Правая часть правила"),
-                                fieldWithPath("automaton.states[].items[].dotIndex")
-                                        .description("Позиция точки в правой части правила"),
-                                fieldWithPath("automaton.states[].items[].lookAheadSymbol")
-                                        .description("Терминальный символ (или символ завершения строки `⊣`)"),
-                                fieldWithPath("automaton.transitions")
-                                        .description("<<automatonTransitions,Переходы>> между состояниями автомата"),
-                                fieldWithPath("automaton.transitions[].from")
-                                        .description("Исходное состояние"),
-                                fieldWithPath("automaton.transitions[].to")
-                                        .description("Целевое состояние"),
-                                fieldWithPath("automaton.transitions[].through")
-                                        .description("Символ перехода"),
-                                fieldWithPath("buildLog")
-                                        .description("<<buildLog,Лог построения автомата>>"),
-                                fieldWithPath("buildLog.operations")
-                                        .description("Последовательность операций построения"),
-                                fieldWithPath("buildLog.operations[].message")
-                                        .description("Человекочитаемое описание операции"),
-                                fieldWithPath("buildLog.operations[].level")
-                                        .description("Тип операции"),
-                                fieldWithPath("buildLog.operations[].name")
-                                        .description("Имя операции"),
-                                fieldWithPath("buildLog.operations[].stateName")
-                                        .optional()
-                                        .description("**Необязательное.** Имя состояния"),
-                                fieldWithPath("buildLog.operations[].state")
-                                        .optional()
-                                        .description("**Необязательное.** Состояние, к которому применяется операция"),
-                                fieldWithPath("buildLog.operations[].item")
-                                        .optional()
-                                        .description("**Необязательное.** LR-пункт, связанный с операцией"),
-                                fieldWithPath("buildLog.operations[].item.rule.left")
-                                        .optional()
-                                        .description("**Необязательное.** Левая часть правила пункта"),
-                                fieldWithPath("buildLog.operations[].item.rule.right")
-                                        .optional()
-                                        .description("**Необязательное.** Правая часть правила пункта"),
-                                fieldWithPath("buildLog.operations[].item.dotIndex")
-                                        .optional()
-                                        .description("**Необязательное.** Позиция точки"),
-                                fieldWithPath("buildLog.operations[].item.lookAheadSymbol")
-                                        .optional()
-                                        .description("**Необязательное.** Терминальный символ (или символ завершения строки `⊣`)"),
-                                fieldWithPath("buildLog.operations[].from")
-                                        .optional()
-                                        .description("**Необязательное.** Исходное состояние перехода"),
-                                fieldWithPath("buildLog.operations[].to")
-                                        .optional()
-                                        .description("**Необязательное.** Целевое состояние перехода"),
-                                fieldWithPath("buildLog.operations[].through")
-                                        .optional()
-                                        .description("**Необязательное.** Символ перехода"))));
+                        responseHeaders(CONTENT_TYPE_JSON_HEADER),
+                        responseFields(Stream.concat(LR1_AUTOMATON_DTO.stream(), BUILD_LR1_LOG_DTO.stream()).toList())));
     }
 }
--- src/test/java/ru/urfu/lrviz/api/rest/docs/DocumentationConstants.java
+++ src/test/java/ru/urfu/lrviz/api/rest/docs/DocumentationConstants.java
@@ -1,6 +1,12 @@
 package ru.urfu.lrviz.api.rest.docs;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.headers.HeaderDescriptor;
+import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.restdocs.payload.FieldDescriptor;
+
+import java.util.List;
 
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
@@ -9,15 +15,81 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
  * @since 04.03.2026
  */
 public class DocumentationConstants {
-    public static final FieldDescriptor[] GRAMMAR_DTO = {
+
+    public static final HeaderDescriptor ACCEPT_JSON_HEADER = HeaderDocumentation.headerWithName(HttpHeaders.ACCEPT).description("Всегда `%s`".formatted(MediaType.APPLICATION_JSON_VALUE));
+    public static final HeaderDescriptor CONTENT_TYPE_JSON_HEADER = HeaderDocumentation.headerWithName(HttpHeaders.CONTENT_TYPE).description("Всегда %s".formatted(MediaType.APPLICATION_JSON));
+
+    public static final List<FieldDescriptor> GRAMMAR_DTO = List.of(
             fieldWithPath("terminals").description("Список <<terminals,терминалов>> грамматики."),
             fieldWithPath("nonTerminals").description("Список <<nonTerminals,нетерминалов>> грамматики."),
             fieldWithPath("rules").description("Список <<rules,правил>> грамматики"),
             fieldWithPath("rules[].left").description("Нетерминал левой части правила грамматики"),
             fieldWithPath("rules[].right").description("Символы правой части правила грамматики"),
-            fieldWithPath("startSymbol").description("<<startSymbol,Аксиома>> грамматики (нетерминал)")};
+            fieldWithPath("startSymbol").description("<<startSymbol,Аксиома>> грамматики (нетерминал)"));
+
+    public static final List<FieldDescriptor> BUILD_LR0_LOG_DTO = List.of(
+            fieldWithPath("buildLog").description("<<buildLog,Лог построения автомата>>"),
+            fieldWithPath("buildLog.operations").description("Последовательность операций построения"),
+            fieldWithPath("buildLog.operations[].message").description("Человекочитаемое описание операции"),
+            fieldWithPath("buildLog.operations[].level").description("Тип операции"),
+            fieldWithPath("buildLog.operations[].name").description("Имя операции"),
+            fieldWithPath("buildLog.operations[].stateName").optional().description("**Необязательное.** Имя состояния"),
+            fieldWithPath("buildLog.operations[].state").optional().description("**Необязательное.** Состояние, к которому применяется операция"),
+            fieldWithPath("buildLog.operations[].item").optional().description("**Необязательное.** LR-пункт, связанный с операцией"),
+            fieldWithPath("buildLog.operations[].item.rule.left").optional().description("**Необязательное.** Левая часть правила пункта"),
+            fieldWithPath("buildLog.operations[].item.rule.right").optional().description("**Необязательное.** Правая часть правила пункта"),
+            fieldWithPath("buildLog.operations[].item.dotIndex").optional().description("**Необязательное.** Позиция точки"),
+            fieldWithPath("buildLog.operations[].from").optional().description("**Необязательное.** Исходное состояние перехода"),
+            fieldWithPath("buildLog.operations[].to").optional().description("**Необязательное.** Целевое состояние перехода"),
+            fieldWithPath("buildLog.operations[].through").optional().description("**Необязательное.** Символ перехода"));
+
+    public static final List<FieldDescriptor> BUILD_LR1_LOG_DTO = List.of(
+            fieldWithPath("buildLog").description("<<buildLog,Лог построения автомата>>"),
+            fieldWithPath("buildLog.operations").description("Последовательность операций построения"),
+            fieldWithPath("buildLog.operations[].message").description("Человекочитаемое описание операции"),
+            fieldWithPath("buildLog.operations[].level").description("Тип операции"),
+            fieldWithPath("buildLog.operations[].name").description("Имя операции"),
+            fieldWithPath("buildLog.operations[].stateName").optional().description("**Необязательное.** Имя состояния"),
+            fieldWithPath("buildLog.operations[].state").optional().description("**Необязательное.** Состояние, к которому применяется операция"),
+            fieldWithPath("buildLog.operations[].item").optional().description("**Необязательное.** LR-пункт, связанный с операцией"),
+            fieldWithPath("buildLog.operations[].item.rule.left").optional().description("**Необязательное.** Левая часть правила пункта"),
+            fieldWithPath("buildLog.operations[].item.rule.right").optional().description("**Необязательное.** Правая часть правила пункта"),
+            fieldWithPath("buildLog.operations[].item.dotIndex").optional().description("**Необязательное.** Позиция точки"),
+            fieldWithPath("buildLog.operations[].item.lookAheadSymbol").optional().description("**Необязательное.** Терминальный символ (или символ завершения строки `⊣`)"),
+            fieldWithPath("buildLog.operations[].from").optional().description("**Необязательное.** Исходное состояние перехода"),
+            fieldWithPath("buildLog.operations[].to").optional().description("**Необязательное.** Целевое состояние перехода"),
+            fieldWithPath("buildLog.operations[].through").optional().description("**Необязательное.** Символ перехода"));
+
+    public static final List<FieldDescriptor> LR0_AUTOMATON_DTO = List.of(
+            fieldWithPath("automaton").description("Построенный <<automaton,LR(0)-автомат>>"),
+            fieldWithPath("automaton.states").description("<<automatonStates,Состояния>> автомата"),
+            fieldWithPath("automaton.states[].name").description("Имя состояния"),
+            fieldWithPath("automaton.states[].items").description("<<lrItems,LR(0)-пункты>> в состоянии"),
+            fieldWithPath("automaton.states[].items[].rule").description("Правило грамматики пункта"),
+            fieldWithPath("automaton.states[].items[].rule.left").description("Левая часть правила"),
+            fieldWithPath("automaton.states[].items[].rule.right").description("Правая часть правила"),
+            fieldWithPath("automaton.states[].items[].dotIndex").description("Позиция точки в правой части правила"),
+            fieldWithPath("automaton.transitions").description("<<automatonTransitions,Переходы>> между состояниями автомата"),
+            fieldWithPath("automaton.transitions[].from").description("Исходное состояние"),
+            fieldWithPath("automaton.transitions[].to").description("Целевое состояние"),
+            fieldWithPath("automaton.transitions[].through").description("Символ перехода"));
+
+    public static final List<FieldDescriptor> LR1_AUTOMATON_DTO = List.of(
+            fieldWithPath("automaton").description("Построенный <<automaton,LR(0)-автомат>>"),
+            fieldWithPath("automaton.states").description("<<automatonStates,Состояния>> автомата"),
+            fieldWithPath("automaton.states[].name").description("Имя состояния"),
+            fieldWithPath("automaton.states[].items").description("<<lrItems,LR(0)-пункты>> в состоянии"),
+            fieldWithPath("automaton.states[].items[].rule").description("Правило грамматики пункта"),
+            fieldWithPath("automaton.states[].items[].rule.left").description("Левая часть правила"),
+            fieldWithPath("automaton.states[].items[].rule.right").description("Правая часть правила"),
+            fieldWithPath("automaton.states[].items[].dotIndex").description("Позиция точки в правой части правила"),
+            fieldWithPath("automaton.states[].items[].lookAheadSymbol").description("Терминальный символ (или символ завершения строки `⊣`)"),
+            fieldWithPath("automaton.transitions").description("<<automatonTransitions,Переходы>> между состояниями автомата"),
+            fieldWithPath("automaton.transitions[].from").description("Исходное состояние"),
+            fieldWithPath("automaton.transitions[].to").description("Целевое состояние"),
+            fieldWithPath("automaton.transitions[].through").description("Символ перехода"));
+
 
     private DocumentationConstants() {
-
     }
 }
