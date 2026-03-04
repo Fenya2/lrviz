8ab6c0a0f13fee56d42dfc54f5f760fcce180d21
fenya <fenya74.09@gmail.com>
2026-03-05 02:36:35 +0500

    Обновил документацию по рендеру автоматов

--- src/test/java/ru/urfu/lrviz/api/rest/docs/RenderLR1DocsTest.java
+++ src/test/java/ru/urfu/lrviz/api/rest/docs/RenderLR1DocsTest.java
@@ -4,27 +4,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-
-import java.util.stream.Stream;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.http.HttpHeaders.ACCEPT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.urfu.lrviz.api.VersionsConstants.V1;
 import static ru.urfu.lrviz.api.rest.docs.snippets.DocumentationConstants.*;
-import static ru.urfu.lrviz.core.GrammarExamples.*;
+import static ru.urfu.lrviz.api.rest.docs.snippets.ImageSnippet.responseImagePng;
+import static ru.urfu.lrviz.core.GrammarExamples.G_8;
+import static ru.urfu.lrviz.core.GrammarExamples.getJsonDto;
 
 /**
  * @author fenya
- * @since 23.02.2026
+ * @since 05.03.2026
  */
-class BuildLR1DocsTest extends AbstractMethodDocsTest {
+class RenderLR1DocsTest extends AbstractMethodDocsTest {
     private static final String DOCUMENTED_PATH = "/api/{version}/build/lr1";
 
     @Override
@@ -44,20 +45,23 @@ class BuildLR1DocsTest extends AbstractMethodDocsTest {
 
     @Override
     protected String getProduces() {
-        return APPLICATION_JSON_VALUE;
+        return IMAGE_PNG_VALUE;
     }
 
     @Test
-    void buildLr1() throws Exception {
+    void renderLr1() throws Exception {
         this.mockMvc.perform(post(DOCUMENTED_PATH, V1)
-                        .header(ACCEPT, APPLICATION_JSON_VALUE)
+                        .header(ACCEPT, IMAGE_PNG_VALUE)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(getJsonDto(G_7)))
+                        .content(getJsonDto(G_8)))
                 .andExpect(status().isOk())
-                .andDo(MockMvcRestDocumentation.document(getSnippetPath(), preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-                        requestHeaders(ACCEPT_JSON_HEADER),
+                .andDo(MvcResult::getAsyncResult)
+                .andDo(MockMvcRestDocumentation.document(getSnippetPath(),
+                        preprocessRequest(),
+                        preprocessResponse(),
+                        requestHeaders(CONTENT_TYPE_PNG_HEADER),
                         requestFields(GRAMMAR_DTO),
                         responseHeaders(CONTENT_TYPE_JSON_HEADER),
-                        responseFields(Stream.concat(LR1_AUTOMATON_DTO.stream(), BUILD_LR1_LOG_DTO.stream()).toList())));
+                        responseImagePng("renderLr1")));
     }
 }
