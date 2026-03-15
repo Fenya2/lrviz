d4c8d4ac4c0c31003ac0242063079cc7766e448c
fenya <fenya74.09@gmail.com>
2026-03-15 19:50:57 +0500

    Добавил документацию к построению LALR(1)-автомата

---
+++ src/test/java/ru/urfu/lrviz/api/rest/docs/BuildLALR1DocsTest.java
@@ -0,0 +1,63 @@
+package ru.urfu.lrviz.api.rest.docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.urfu.lrviz.api.VersionsConstants.V1;
+import static ru.urfu.lrviz.api.rest.docs.snippets.DocumentationConstants.*;
+import static ru.urfu.lrviz.core.GrammarExamples.G_9;
+import static ru.urfu.lrviz.core.GrammarExamples.getAsJson;
+
+/**
+ *
+ * @author fenya
+ * @since 15.03.2026
+ */
+class BuildLALR1DocsTest extends AbstractMethodDocsTest {
+    private static final String DOCUMENTED_PATH = "/api/{version}/build/lalr1";
+
+    @Override
+    protected HttpMethod getDocumentedMethod() {
+        return HttpMethod.POST;
+    }
+
+    @Override
+    protected String getDocumentedVersion() {
+        return V1;
+    }
+
+    @Override
+    protected String getDocumentedPath() {
+        return DOCUMENTED_PATH;
+    }
+
+    @Override
+    protected String getProduces() {
+        return APPLICATION_JSON_VALUE;
+    }
+
+    @Test
+    void document() throws Exception {
+        this.mockMvc.perform(post(DOCUMENTED_PATH, V1)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getAsJson(G_9)))
+                .andExpect(status().isOk())
+                .andDo(MockMvcRestDocumentation.document(getSnippetPath(), preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                        requestHeaders(ACCEPT_JSON_HEADER),
+                        pathParameters(VERSION_PARAMETER),
+                        BUILD_LALR_AUTOMATON_REQUEST,
+                        responseHeaders(CONTENT_TYPE_JSON_HEADER),
+                        BUILD_LALR1_AUTOMATON_RESPONSE));
+    }
+}
